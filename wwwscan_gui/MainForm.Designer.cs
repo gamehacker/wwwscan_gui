e44f6d0224cc9db6423d2149bab5bf0f5c36ad5f
luciker <luciker@sohu.com>
2014-10-20 16:51:22 +0800

    init

---
+++ wwwscan_gui/MainForm.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace wwwscan_gui
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_startscan = new System.Windows.Forms.Button();
+            this.textBox_target = new System.Windows.Forms.TextBox();
+            this.textBox_port = new System.Windows.Forms.TextBox();
+            this.textBox_threadnum = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
+            this.checkBox9 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button_startscan
+            // 
+            this.button_startscan.Location = new System.Drawing.Point(122, 297);
+            this.button_startscan.Name = "button_startscan";
+            this.button_startscan.Size = new System.Drawing.Size(75, 23);
+            this.button_startscan.TabIndex = 0;
+            this.button_startscan.Text = "Scan";
+            this.button_startscan.UseVisualStyleBackColor = true;
+            this.button_startscan.Click += new System.EventHandler(this.button_startscan_Click);
+            // 
+            // textBox_target
+            // 
+            this.textBox_target.Location = new System.Drawing.Point(12, 12);
+            this.textBox_target.Name = "textBox_target";
+            this.textBox_target.Size = new System.Drawing.Size(251, 21);
+            this.textBox_target.TabIndex = 1;
+            this.textBox_target.Text = "www.test.com/admin";
+            // 
+            // textBox_port
+            // 
+            this.textBox_port.Location = new System.Drawing.Point(269, 12);
+            this.textBox_port.Name = "textBox_port";
+            this.textBox_port.Size = new System.Drawing.Size(39, 21);
+            this.textBox_port.TabIndex = 2;
+            this.textBox_port.Text = "80";
+            // 
+            // textBox_threadnum
+            // 
+            this.textBox_threadnum.Location = new System.Drawing.Point(314, 12);
+            this.textBox_threadnum.Name = "textBox_threadnum";
+            this.textBox_threadnum.Size = new System.Drawing.Size(39, 21);
+            this.textBox_threadnum.TabIndex = 3;
+            this.textBox_threadnum.Text = "10";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.checkBox7);
+            this.panel1.Controls.Add(this.checkBox8);
+            this.panel1.Controls.Add(this.checkBox9);
+            this.panel1.Controls.Add(this.checkBox4);
+            this.panel1.Controls.Add(this.checkBox5);
+            this.panel1.Controls.Add(this.checkBox6);
+            this.panel1.Controls.Add(this.checkBox3);
+            this.panel1.Controls.Add(this.checkBox2);
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Location = new System.Drawing.Point(12, 67);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(341, 193);
+            this.panel1.TabIndex = 13;
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Location = new System.Drawing.Point(215, 121);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(48, 16);
+            this.checkBox7.TabIndex = 21;
+            this.checkBox7.Text = "edit";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // checkBox8
+            // 
+            this.checkBox8.AutoSize = true;
+            this.checkBox8.Location = new System.Drawing.Point(215, 88);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Size = new System.Drawing.Size(36, 16);
+            this.checkBox8.TabIndex = 20;
+            this.checkBox8.Text = "db";
+            this.checkBox8.UseVisualStyleBackColor = true;
+            // 
+            // checkBox9
+            // 
+            this.checkBox9.AutoSize = true;
+            this.checkBox9.Location = new System.Drawing.Point(215, 55);
+            this.checkBox9.Name = "checkBox9";
+            this.checkBox9.Size = new System.Drawing.Size(42, 16);
+            this.checkBox9.TabIndex = 19;
+            this.checkBox9.Text = "rar";
+            this.checkBox9.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(131, 121);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(42, 16);
+            this.checkBox4.TabIndex = 18;
+            this.checkBox4.Text = "dir";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(131, 88);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(42, 16);
+            this.checkBox5.TabIndex = 17;
+            this.checkBox5.Text = "txt";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(131, 55);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(42, 16);
+            this.checkBox6.TabIndex = 16;
+            this.checkBox6.Text = "jsp";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(47, 121);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(42, 16);
+            this.checkBox3.TabIndex = 15;
+            this.checkBox3.Text = "php";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(47, 88);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(48, 16);
+            this.checkBox2.TabIndex = 14;
+            this.checkBox2.Text = "aspx";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(47, 55);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(42, 16);
+            this.checkBox1.TabIndex = 13;
+            this.checkBox1.Text = "asp";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(365, 332);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox_threadnum);
+            this.Controls.Add(this.textBox_port);
+            this.Controls.Add(this.textBox_target);
+            this.Controls.Add(this.button_startscan);
+            this.Name = "MainForm";
+            this.Text = "wwwscan gui——by luciker";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_startscan;
+        private System.Windows.Forms.TextBox textBox_target;
+        private System.Windows.Forms.TextBox textBox_port;
+        private System.Windows.Forms.TextBox textBox_threadnum;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.CheckBox checkBox8;
+        private System.Windows.Forms.CheckBox checkBox9;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ wwwscan_gui/MainForm.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace wwwscan_gui
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //byte[] bytes = new WebClient().DownloadData("http://" + this.txtTarget.Text);
+                //string str = Encoding.Default.GetString(bytes);
+                //string[] strArray = new string[] { ".asp", ".aspx", ".php", ".jsp" };
+                //for (int i = 0; i < 4; i++)
+                //{
+                //    if (str.IndexOf(strArray[i]) != -1)
+                //    {
+                //        this.lblType.Text = "可能为" + strArray[i].Substring(1);
+                //        return;
+                //    }
+                //}
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("不能确定脚本类型");
+            }
+        }
+
+    
+
+        private bool changeList()
+        {
+            FileInfo info = new FileInfo(this.getType());
+            if (System.IO.File.Exists("cgi.list"))
+            {
+                System.IO.File.Delete("cgi.list");
+            }
+            info.CopyTo("cgi.list");
+            return true;
+        }
+
+
+        private string getPath()
+        {
+            //if (this.rBmain.Checked)
+            //{
+            //    return "";
+            //}
+            //return ("/" + this.cBPath.Text + "/");
+            throw new NotImplementedException();
+        }
+
+        private int getPort()
+        {
+            //return int.Parse(this.txtPort.Text);
+            throw new NotImplementedException();
+        }
+
+        private int getThread()
+        {
+            return int.Parse(this.textBox_threadnum.Text);
+        }
+
+        private string getType()
+        {
+            //int count = this.groupBox2.Controls.Count;
+            //for (int i = 0; i < count; i++)
+            //{
+            //    if (((RadioButton)this.groupBox2.Controls[i]).Checked)
+            //    {
+            //        return ("dic/" + ((RadioButton)this.groupBox2.Controls[i]).Text + ".list");
+            //    }
+            //}
+            //return "";
+            throw new NotImplementedException();
+        }
+
+
+        private void p_Exited(object sender, EventArgs e)
+        {
+            //MessageBox.Show(this.txtTarget.Text + "扫描完毕,请查看程序目录下的报告");
+        }
+
+        private void rBmain_CheckedChanged(object sender, EventArgs e)
+        {
+            //this.cBPath.Enabled = false;
+        }
+
+        private void rBOther_CheckedChanged(object sender, EventArgs e)
+        {
+            //this.cBPath.Enabled = true;
+        }
+
+        private void txtPort_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!((e.KeyChar == '\b') || char.IsDigit(e.KeyChar)))
+            {
+                e.Handled = true;
+                MessageBox.Show("端口号只能是数字");
+            }
+        }
+
+        private void txtThread_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!((e.KeyChar == '\b') || char.IsDigit(e.KeyChar)))
+            {
+                e.Handled = true;
+                MessageBox.Show("线程数只能是数字");
+            }
+        }
+
+        private void button_startscan_Click(object sender, EventArgs e)
+        {
+            this.changeList();
+            Process process = new Process
+            {
+                StartInfo = { FileName = "wwwscan.exe" }
+            };
+            if (this.getPath() == "")
+            {
+                //process.StartInfo.Arguments = this.txtTarget.Text + " -p " + this.txtPort.Text + " -m " + this.txtThread.Text;
+            }
+            else
+            {
+                //process.StartInfo.Arguments = this.txtTarget.Text + " -r " + this.getPath() + " -p " + this.txtPort.Text + " -m " + this.txtThread.Text;
+            }
+            process.EnableRaisingEvents = true;
+            process.Exited += new EventHandler(this.p_Exited);
+            process.Start();
+        }
+    }
+}
